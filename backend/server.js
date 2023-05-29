7a20a256262bfb962918c66100843968056f9fb2
Anders Strömberg <coltlaboy@yahoo.se>
2023-05-29 13:04:08 +0200

    Updated server file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
+import path from 'path'
 import cookieParser from 'cookie-parser'
 import { notFound, errorHandler } from './middelware/errorMiddleware.js'
 import connectDB from './config/db.js'
@@ -17,9 +18,18 @@ app.use(cookieParser())
 
 app.use('/api/users', userRoutes)
 
-app.get('/', (req, res) => {
-  res.send('Server is ready')
-})
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve()
+  app.use(express.static(path.join(__dirname, 'frontend/dist')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('Server is ready')
+  })
+}
 
 // Custom error handler
 app.use(notFound)
